0d3049a12a139e87edc69877de536f486378ed7c
Jonathan <jonathan.trhodes07@gmail.com>
2021-10-05 10:56:17 -0500

    finished order exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -4,12 +4,20 @@ SELECT * FROM employees
 WHERE (first_name LIKE 'Irena'
     OR first_name LIKE 'Vidya'
     OR first_name LIKE 'Maya')
-    AND gender ='M'
+#   AND gender ='M'
+    ORDER BY first_name, last_name;
+
+SELECT * FROM employees
+WHERE (first_name LIKE 'Irena'
+    OR first_name LIKE 'Vidya'
+    OR first_name LIKE 'Maya')
+#   AND gender ='M'
+ORDER BY last_name, first_name;
 
 
 
 SELECT * FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE '%E%' ORDER BY emp_no;
 
 SELECT * FROM employees
 WHERE last_name LIKE '%Q%';
